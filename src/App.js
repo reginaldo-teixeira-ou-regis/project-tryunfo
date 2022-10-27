dae57f73d250777b834ce383808751811a53d08a
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-10-27 03:41:23 -0300

    Requisite 5 complete

--- src/App.js
+++ src/App.js
@@ -23,12 +23,55 @@ class App extends React.Component {
 
   }
 
+  ValidationInput = () => {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare } = this.state;
+
+    const errorCases = [
+      !cardName.length,
+      !cardDescription.length,
+      !cardImage.length,
+      !cardRare.length,
+    ];
+
+    const validationTextLength = errorCases.every((error) => error !== true);
+
+    const totalSun = 211;
+    const pointsMax = 90;
+    const newCardAttr1 = Number(cardAttr1);
+    const newCardAttr2 = Number(cardAttr2);
+    const newCardAttr3 = Number(cardAttr3);
+    const maxPoint = newCardAttr1 > pointsMax
+    || newCardAttr2 > pointsMax
+    || newCardAttr3 > pointsMax
+    || newCardAttr1 < 0
+    || newCardAttr2 < 0
+    || newCardAttr3 < 0;
+
+    if ((newCardAttr1 + newCardAttr2 + newCardAttr3) < totalSun
+      && validationTextLength
+      && !maxPoint) {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+      return;
+    }
+    this.setState({
+      isSaveButtonDisabled: true,
+    });
+  };
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.ValidationInput);
   };
 
   render() {
@@ -42,6 +85,7 @@ class App extends React.Component {
       cardTrunfo,
       /* hasTrunfo, */
       isSaveButtonDisabled } = this.state;
+
     const defaultProps = {
       cardName,
       cardDescription,
